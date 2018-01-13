8b3059bdc014a78ea370a4288ab269af4fcdea4b
834712719 <834712719@qq.com>
2018-01-13 19:23:04 +0800

    提交文件

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+  header("Content-type:text/html;charset=utf8");
+  define("APP_PATH","application/");
+  define("BIND_MODULE","Home");
+  define("APP_DEBUG",true);
+  define("BUILD_DIR_SECURE", false);
+  
+  include_once "ThinkPHP/ThinkPHP.php";
+
+?>
